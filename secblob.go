ffcdb58ae4ff5b8c1b43076c9313b891c1b04ffa
theirish81 <theirish81@gmail.com>
2024-06-09 22:04:59 +0200

    meaningless header set

--- secblob.go
+++ secblob.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"github.com/getkin/kin-openapi/openapi3filter"
 	"github.com/labstack/echo/v4"
-	"net/http"
 )
 
 func SecBlob(ctx echo.Context, status int, dto any) error {
@@ -24,8 +23,6 @@ func SecBlob(ctx echo.Context, status int, dto any) error {
 	}
 
 	rvi, ok := ctx.Get(requestValidationInputAttr).(*openapi3filter.RequestValidationInput)
-	h := http.Header{}
-	h.Set(contentTypeHeaderName, contentTypeJsonValue)
 	if ok {
 		responseValidationInput := &openapi3filter.ResponseValidationInput{
 			RequestValidationInput: rvi,
